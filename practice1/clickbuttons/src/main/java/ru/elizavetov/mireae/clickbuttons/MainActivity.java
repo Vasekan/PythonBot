36f1324835f99cf56ca711fcdbccfd450440675a
Vasiliy Elizavetov <elizavetov65@gmail.com>
2022-03-10 11:21:46 +0300

    first commit

---
+++ practice1/clickbuttons/src/main/java/ru/elizavetov/mireae/clickbuttons/MainActivity.java
@@ -0,0 +1,43 @@
+package ru.elizavetov.mireae.clickbuttons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvOut;
+    private Button buttonOK;
+    private Button buttonCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        buttonOK = (Button) findViewById(R.id.btnOk);
+        buttonCancel = (Button) findViewById(R.id.btnCancel);
+        View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvOut.setText("Нажата кнопка Ok");
+            }
+        };
+
+        buttonOK.setOnClickListener(oclBtnOk);
+
+        View.OnClickListener oclBtnCancel = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvOut.setText("Нажата кнопка Cancel");
+            }
+        };
+
+        buttonCancel.setOnClickListener(oclBtnCancel);
+
+    }
+
+}
