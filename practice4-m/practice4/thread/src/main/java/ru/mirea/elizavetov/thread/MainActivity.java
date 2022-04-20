eb69ab2b5d76ec52498e30f9997fe15a9fdb65ca
Vasiliy Elizavetov <elizavetov65@gmail.com>
2022-04-20 19:24:31 +0300

    pr4

---
+++ practice4-m/practice4/thread/src/main/java/ru/mirea/elizavetov/thread/MainActivity.java
@@ -0,0 +1,34 @@
+package ru.mirea.elizavetov.thread;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText lessonsCountInput;
+    private EditText daysCountInput;
+    private TextView resultTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lessonsCountInput = (EditText) findViewById(R.id.lesson_count);
+        daysCountInput = (EditText) findViewById(R.id.day_count);
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+        Button showResultsButton = (Button) findViewById(R.id.show_result);
+        showResultsButton.setOnClickListener((view) -> {
+            Thread thread = new Thread(() -> {
+                double lessonsCount = Double.parseDouble(lessonsCountInput.getText().toString());
+                double daysCount = Double.parseDouble(daysCountInput.getText().toString());
+                double lessonsPerDay = lessonsCount / daysCount;
+                runOnUiThread(() -> resultTextView.setText(String.valueOf(lessonsPerDay)));
+            });
+            thread.start();
+        });
+    }
+}
