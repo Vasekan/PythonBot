378ba67fc9df4916e8678098bb61b5841afb5e95
Vasiliy Elizavetov <101211165+Vasekan@users.noreply.github.com>
2022-05-29 02:31:36 +0300

    Delete practice9-m directory

--- practice9-m/MireaProject/app/src/main/java/ru/mirea/elizavetov/mireaproject/ui/story/AppDatebase.java
+++
@@ -1,8 +0,0 @@
-package ru.mirea.elizavetov.mireaproject.ui.story;
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Story.class}, version = 2)
-public abstract class AppDatebase extends RoomDatabase{
-    public abstract StoryDao storyDao();
-}
